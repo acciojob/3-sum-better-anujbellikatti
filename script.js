e3993b8f2e36397f2c1dd785da85300c952b8a28
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-08-18 20:40:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let c=0;
+  for(let i=0;i<arr.length;i++){
+	  for(let j=i+1;j<arr.length;j++){
+		  for(let k=j+1;k<arr.length;k++){
+			  if(arr[i]+arr[j]+arr[k]==target){
+				c++;	  
+			  }
+		  }
+	  }
+  }
+	console.log(c);
 }
 
 module.exports = threeSum;
