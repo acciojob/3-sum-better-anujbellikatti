219398a6a1abfe42bf88b18d9bf3cc71815b002f
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-08-18 22:12:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function threeSum(arr, target) {
 // write your code here
 	let c=0;
-  for(let i=0;i<arr.length-2;i++){
-	  for(let j=i+1;j<arr.length-1;j++){
-		  for(let k=j+1;k<arr.length;k++){
-			  if(arr[i]+arr[j]+arr[k]==target){
+  for(let i=0;i<arr.length;i++){
+	  for(let j=0;j<arr.length;j++){
+		  for(let k=0;k<arr.length;k++){
+			  if(arr[i]+arr[j]+arr[k]==target &&i!==j && j!==k){
 				c++;	  
 			  }
 		  }
